b518d5ac4faf66065a40dd90733135dc08e5690e
luisantoniomarques <luisantoniomarques@yahoo.co.uk>
2026-02-18 15:47:44 +0000

    Add files via upload

---
+++ Controllers/RentalController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using MovieRental.Movie;
+using MovieRental.Rental;
+
+namespace MovieRental.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RentalController : ControllerBase
+    {
+
+        private readonly IRentalFeatures _features;
+
+        public RentalController(IRentalFeatures features)
+        {
+            _features = features;
+        }
+
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Rental.Rental rental)
+        {
+	        return Ok(_features.Save(rental));
+        }
+
+        // Endpoint to filter rentals by customer name.
+        [HttpGet]
+        public IActionResult Get([FromBody] Rental.Rental rental)
+        {
+            return Ok(_features.GetRentalsByCustomerName(rental.Client.Name));
+        }
+    }
+}
