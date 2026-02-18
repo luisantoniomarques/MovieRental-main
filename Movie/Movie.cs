dee0dfa5499073775491d9c2c69bab1fd1ffbbb7
luisantoniomarques <luisantoniomarques@yahoo.co.uk>
2026-02-18 15:48:50 +0000

    Add files via upload

---
+++ Movie/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieRental.Movie
+{
+	public class Movie
+	{
+		[Key]
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public int Price { get; set; } // I added price per day
+
+    }
+}
---
+++ Movie/MovieFeatures.cs
@@ -0,0 +1,33 @@
+﻿using MovieRental.Data;
+using System.Runtime.InteropServices.Marshalling;
+
+namespace MovieRental.Movie
+{
+	public class MovieFeatures : IMovieFeatures
+	{
+		private readonly MovieRentalDbContext _movieRentalDb;
+
+		public MovieFeatures(MovieRentalDbContext movieRentalDb)
+		{
+			_movieRentalDb = movieRentalDb;
+		}
+		
+		public Movie Save(Movie movie)
+		{
+			_movieRentalDb.Movies.Add(movie);
+			_movieRentalDb.SaveChanges();
+			return movie;
+		}
+
+        // TODO: tell us what is wrong in this method? Forget about the async, what other concerns do you have?
+        // Como tem tendencia a ser uma operação demorada, o ideal seria usar async/await para não bloquear a thread principal.
+		// Além disso, o método não tem tratamento de erros, o que pode levar a falhas silenciosas ou crashes se algo der errado durante a consulta ao banco de dados.
+		// Também seria interessante considerar a paginação dos resultados para evitar carregar uma grande quantidade de dados na memória de uma só vez, caso haja muitos filmes no banco de dados.
+        public List<Movie> GetAll()
+		{
+			return _movieRentalDb.Movies.ToList();
+		}
+
+
+	}
+}
