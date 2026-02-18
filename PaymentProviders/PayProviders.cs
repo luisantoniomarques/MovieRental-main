2d2b297c5bf5e7f0d5e3ef553ca1311deee6c120
luisantoniomarques <luisantoniomarques@yahoo.co.uk>
2026-02-18 15:50:25 +0000

    Add files via upload

---
+++ PaymentProviders/PayProviders.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+
+namespace MovieRental.PaymentProviders
+{
+    public class PayProviders
+    {
+        //A class that brings together the various payers of services.
+        public MbWayProvider mbProv;
+        public PayPalProvider PayPal;
+        // Other providers
+    }
+}
